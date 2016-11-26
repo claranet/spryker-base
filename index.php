8f97f1891bddfcb353ad5538763f3e2e5699ec17
Fabian Dörk <fabian.doerk@de.clara.net>
2016-11-26 15:44:19 +0100

    adds first batch of necessary configuration files (WIP)
    The base set of configuration files in order to run a spryker based
    shop. This is work in progress because further spryker adaption needs to
    be done.

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Spryker container framework on <pre>".system("hostname -f")."</pre> with ip address <pre>".system("getent hosts `hostname` | awk '{print $1}'")."</pre>";
+
+phpinfo();
