4aa9f01f8368f001d06ee8441fc033598bae1db8
Tony Wolf <tony.wolf@de.clara.net>
2017-04-12 20:58:05 +0200

    migrate install_php.sh to support alpine apk/php:x.x-fpm-alpine specifics
    Make use of docker-php-ext-(configure|install|enable) and add support for
    special php extensions like
    
    * xcache
    * gd
    * imagick
    
    Use apk, but clean up the installation after building extensions is done.

--- build/install_php.sh
+++ build/install_php.sh
@@ -9,7 +9,6 @@
 
 
 # include helper functions
-source ./build_library.sh
 source ./functions.sh
 
 # abort on error
@@ -20,22 +19,55 @@ set -e
 #  Install PHP extensions
 #
 
-PHP_DEPENDENCIES="$PHP_EXTENSION_DEPENDENCIES \
-  libfreetype6 \
-  libjpeg62-turbo \
-  libmcrypt \
-  libpng12"
-
-apk install --no-cache $PHP_DEPENDENCIES
-
-# install base PHP extensions
-docker-php-ext-install -j$(nproc) iconv mcrypt
-docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
-docker-php-ext-install -j$(nproc) gd
 
 # FIXME: install extensions, some of them need a special install way
 # write functions to install special extensions
 # same goes for pecl
+
+function install_imagick() {
+  apk add --no-cache --virtual .phpize-deps $PHPIZE_DEPS imagemagick-dev libtool
+  pecl install imagick-3.4.3
+  docker-php-ext-enable -j$(nproc) imagick
+  apk add --no-cache --virtual .imagick-runtime-deps imagemagick
+  apk del .phpize-deps
+}
+
+function install_gd() {
+  apk add --no-cache --virtual .phpize-deps $PHPIZE_DEPS libfreetype6-dev \
+        libjpeg62-turbo-dev \
+        libmcrypt-dev \
+        libpng12-dev
+  
+  docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
+  docker-php-ext-install -j$(nproc) gd
+  apk del .phpize-deps
+}
+
+function install_xcache() {
+  curl -fsSL 'https://xcache.lighttpd.net/pub/Releases/3.2.0/xcache-3.2.0.tar.gz' -o xcache.tar.gz \
+    && mkdir -p /tmp/xcache \
+    && tar -xf xcache.tar.gz -C /tmp/xcache --strip-components=1 \
+    && rm xcache.tar.gz \
+    && docker-php-ext-configure /tmp/xcache --enable-xcache \
+    && docker-php-ext-install /tmp/xcache \
+    && rm -r /tmp/xcache
+}
+
+docker-php-source extract
+
+for ext in $PHP_EXTENSIONS; do
+  
+  case $ext in
+    imagick|gd|xcache)
+      install_$ext
+      ;;
+    *)
+      # try to install unknown extensions as it is possible, that they are part of the core
+      # TODO: check, if the ext is part of the core
+      docker-php-ext-install -j$(nproc) $ext
+  esac
+  
+done
 
 
 #
