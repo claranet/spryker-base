d5f5d13b5c7d69bd193037bef804c5de1723961a
Tony Wolf <tony.wolf@de.clara.net>
2017-05-05 01:37:45 +0200

    fix missing dir /data/logs

--- docker/common.inc.sh
+++ docker/common.inc.sh
@@ -122,7 +122,7 @@ start_services() {
   
   # fix error with missing event log dir
   # TODO: configure log destination to /data/logs/
-  mkdir -p /data/shop/data/$SPRYKER_SHOP_CC/logs/
+  mkdir -p /data/logs $WORKDIR/data/$SPRYKER_SHOP_CC/logs/
   
   # TODO: increase security by making this more granular
   chown -R www-data: /data/logs /data/shop
