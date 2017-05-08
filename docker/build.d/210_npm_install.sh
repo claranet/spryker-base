091f6beeb94ca6879b2982455698bf56edd85ad4
Tony Wolf <tony.wolf@de.clara.net>
2017-05-08 16:20:00 +0200

    adds ability to specify npm OS package dependencies
    Temporary build (npm install) and remaining system dependencies are
    now supported. If an npm package runs a compile job, now you can
    specify build depenencies via: NPM_BUILD_DEPENDENCIES="automake"
    
    If the resulting binary from the build above needs remaining libraries
    on the system, or an npm package requires a special system command, you
    can now specify packages to be installed via filling NPM_DEPENDENCIES="libpng"

--- docker/build.d/210_npm_install.sh
+++ docker/build.d/210_npm_install.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# specify alpinelinux packages via those variables (you may define them in your build.conf)
+# NPM_BUILD_DEPENDENCIES will be deleted after the `npm install` command is finished
+# NPM_DEPENDENCIES will remain on the system
+NPM_BUILD_DEPENDENCIES=${NPM_BUILD_DEPENDENCIES:-}
+NPM_DEPENDENCIES=${NPM_DEPENDENCIES:-}
+
+
+if [ ! -z "$NPM_DEPENDENCIES" ]; then
+  $apk_add --virtual npm_dependencies $NPM_DEPENDENCIES
+fi
+
+if [ ! -z "$NPM_BUILD_DEPENDENCIES" ]; then
+  $apk_add --virtual npm_build_dependencies $NPM_BUILD_DEPENDENCIES
+fi
+
+
 # install dependencies for building asset
 # --with-dev is required to install spryker/oryx (works behind npm run x)
 infoText "Installing required NPM dependencies..."
@@ -13,3 +29,8 @@ for i in `find vendor/ -name 'package.json' | egrep 'assets/(Zed|Yves)/package.j
   $NPM install
   cd $WORKDIR
 done
+
+
+if [ ! -z "$NPM_BUILD_DEPENDENCIES" ]; then
+  apk del npm_build_dependencies
+fi
