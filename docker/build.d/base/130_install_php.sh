68b945f43cb1b94a8543fe4985c21801116be4b6
Tony Fahrion <tony.wolf@de.clara.net>
2017-06-01 10:39:55 +0200

    move more build output to BUILD_LOG

--- docker/build.d/base/130_install_php.sh
+++ docker/build.d/base/130_install_php.sh
@@ -156,24 +156,22 @@ php_install_extensions() {
   docker-php-source delete
 }
 
+php_install_composer() {
+  sectionText "Downloading PHP composer"
+  curl -sS -o /tmp/composer-setup.php https://getcomposer.org/installer
+  curl -sS -o /tmp/composer-setup.sig https://composer.github.io/installer.sig
+  php -r "if (hash('SHA384', file_get_contents('/tmp/composer-setup.php')) !== trim(file_get_contents('/tmp/composer-setup.sig'))) { unlink('/tmp/composer-setup.php'); echo 'Invalid installer' . PHP_EOL; exit(1); }"
+
+  sectionText "Install PHP composer"
+  php /tmp/composer-setup.php --install-dir=/usr/bin/ >> $BUILD_LOG
+
+  # make the installation process of `composer install` faster by parallel downloads
+  composer.phar global require hirak/prestissimo >> $BUILD_LOG
+}
+
 
 php_install_extensions
-
-
-#
-#   Composer
-#
-sectionText "Downloading PHP composer"
-curl -sS -o /tmp/composer-setup.php https://getcomposer.org/installer
-curl -sS -o /tmp/composer-setup.sig https://composer.github.io/installer.sig
-php -r "if (hash('SHA384', file_get_contents('/tmp/composer-setup.php')) !== trim(file_get_contents('/tmp/composer-setup.sig'))) { unlink('/tmp/composer-setup.php'); echo 'Invalid installer' . PHP_EOL; exit(1); }"
-
-sectionText "Install PHP composer"
-php /tmp/composer-setup.php --install-dir=/usr/bin/
-
-# make the installation process of `composer install` faster by parallel downloads
-composer.phar global require hirak/prestissimo
-
+php_install_composer
 
 #
 #  Clean up
