8d3922b8e7d14740f1ffa5e9393d891afa9a436a
Fabian Dörk <fabian.doerk@de.clara.net>
2017-06-14 16:48:18 +0200

    Add explicit disabling of unneeded modules

--- docker/entry.d/100_enable_opcache.sh
+++ docker/entry.d/100_enable_opcache.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 ext=opcache
 if is_true $ENABLE_OPCACHE; then
-  sectionText "Enable PHP extension $ext"
+  sectionText "Enabling PHP extension $ext"
   docker-php-ext-enable $ext >> $BUILD_LOG 2>&1
+else
+  sectionText "Disabling PHP extension $ext"
+  file="/usr/local/etc/php/conf.d/docker-php-ext-opcache.ini"
+  [ -e $file ] && rm $file
 fi
--- docker/entry.d/105_enable_xdebug.sh
+++ docker/entry.d/105_enable_xdebug.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-
-if is_true $ENABLE_XDEBUG; then
-  sectionText "Enable PHP extension xdebug"
-  docker-php-ext-enable xdebug
-  mkdir -pv /tmp/xdebug
-  chmod -v 7777 /tmp/xdebug
+ext=$ext
+if is_true $ENABLE_$ext; then
+  sectionText "Enabling PHP extension $ext"
+  docker-php-ext-enable $ext
+  mkdir -pv /tmp/$ext
+  chmod -v 7777 /tmp/$ext
+else
+  sectionText "Disabling PHP extension $ext"
+  file="/usr/local/etc/php/conf.d/docker-php-ext-$ext.ini"
+  [ -e $file ] && rm $file
 fi >> $BUILD_LOG 2>&1
