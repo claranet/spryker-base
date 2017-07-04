62af0bd4cf2599670e2699070d800426be086249
Fabian Dörk <fabian.doerk@de.clara.net>
2017-07-04 22:48:20 +0200

    Fix minor issue with crontab generation

--- docker/entry.d/205_configure_cron.sh
+++ docker/entry.d/205_configure_cron.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if is_in_list "cron" "$ENABLED_SERVICES"; then
+if is_in_list "crond" "$ENABLED_SERVICES"; then
   sectionText "Configuring crond as the cronjob handler"
   php $WORKDIR/docker/contrib/gen_crontab.php
 fi
