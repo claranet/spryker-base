76f6caf2bdc001c2f68664aeed0da563fc86813f
Fabian Dörk <fabian.doerk@de.clara.net>
2017-07-05 18:40:24 +0200

    Fix travis

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e -o pipefail
+set -x
+[ "$TRAVIS_PULL_REQUEST" == "true" ] && _echo "Pull Requests are not allowed to publish image!" && exit 0
+
+echo "Authenticating to docker hub ..."
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 docker pull $image:$tagci
 docker tag $image:$tagci $image:$tag
