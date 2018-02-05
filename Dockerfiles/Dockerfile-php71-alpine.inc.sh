0dce4c4e2c2f75121062efdf458894dbd14edecf
Christian Cöster <christian.coester@de.clara.net>
2018-02-05 16:59:11 +0100

    remove remaining alpine files AND move Dockerfile data into a single file

--- Dockerfiles/Dockerfile-php71-alpine.inc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -a
-source Dockerfile-common.inc.sh
-source Dockerfile-common-alpine.inc.sh
-
-FROM="FROM php:7.1.10-fpm-alpine"
--- Dockerfiles/Dockerfile-php71-jessie.inc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -a
-source Dockerfile-common.inc.sh
-source Dockerfile-common-debian.inc.sh
-
-FROM="FROM php:7.1.13-fpm-jessie"
