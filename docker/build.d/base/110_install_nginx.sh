3bfc35f116c3a6491b081a1e9eb32563397fbff5
Christian Cöster <christian.coester@de.clara.net>
2018-02-05 17:18:26 +0100

    clean up

--- docker/build.d/base/110_install_nginx.sh
+++ docker/build.d/base/110_install_nginx.sh
@@ -2,15 +2,10 @@
 
 # we are using more_clear_headers to remove client header
 # see: https://github.com/openresty/headers-more-nginx-module
-install_packages nginx-common 
-install_packages nginx-extras 
-install_packages nginx
+install_packages nginx-common nginx-extras nginx
 
 # remove default nginx configuration
 rm /etc/nginx/sites-enabled/default
-
-# remove default vhost config, if favour of our yves/zed vhosts
-#rm /etc/nginx/conf.d/default.conf
 
 # create the required run dir to allow nginx to create its pid file
 mkdir /run/nginx
